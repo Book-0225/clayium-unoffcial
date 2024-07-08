e7fff967d4756ea348ca9dc2cb5aaef9cf125fe1
Book-0225 <honma.h0225@gmail.com>
2024-07-09 06:44:41 +0900

    First Commit

---
+++ src/main/java/com/hiro/clayium_unofficial/main/Clayium_unofficial.java
@@ -0,0 +1,11 @@
+package com.hiro.clayium_unofficial.main;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod("clayium_unofficial")
+public class Clayium_unofficial {
+    public  static final String MOD_ID = "clayium_unofficial";
+    public Clayium_unofficial(){
+
+    }
+}
